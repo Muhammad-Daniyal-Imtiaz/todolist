a7eaffd47d6aa43cca797728c1f804405e9d9729
Mdos1 <muhammaddaniimtiaz@gmail.com>
2023-08-31 18:03:08 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState('');
+  const [n, setN] = useState([
+    { name: 'hadi', age: 23, status: 'React-js Developer' },
+    { name: 'Python-Developer', age: 20, status: 'Gatsby-js Developer' },
+    { name: 'hadi', age: 24, status: 'Next-js Developer' }
+  ]);
+
+  function Add(e) {
+    e.preventDefault(); // Prevents the form from submitting and refreshing the page
+
+    if (name !== '') {
+      const newData = { name: name, age: null, status: '' };
+      setN([...n, newData]);
+      setName('');
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <center> */}
+        <h id='hea'><strong>To do list</strong></h>
+      {/* </center> */}
+      <div>
+        <center>
+          <form>
+            <input id='i1'
+              type="text"
+              placeholder="Enter the text"
+              value={name}
+
+              onChange={(e) => setName(e.target.value)}
+            />
+            <button onClick={Add}>ADD</button>
+          </form>
+        </center>
+      </div>
+
+      <center>
+        {n.map((x, index) => (
+          <div key={index}>
+            <h1>
+              {x.name} {x.age} {x.status}
+            </h1>
+            <hr />
+          </div>
+        ))}
+      </center>
     </div>
   );
 }
